6efaf6a982cb90a300038a5d994bdbc35db02a98
François Termote <francois.termote@gmail.com>
2023-09-27 22:29:35 +0200

    TP recettes, HTML 80%, CSS 10%, JS boutons créés, modale à remplir, input à paramétrer

--- TP-27sept/js/main.js
+++ TP-27sept/js/main.js
@@ -3,6 +3,11 @@ const recipesList = [];
 for (const cle in recipes) {
     recipesList.push(recipes[cle]);
 }
+const recettesKeys = Object.keys(recipes);
+for (let i = 0; i < recipesList.length; i++) {
+    recipesList[i].id = recettesKeys[i];
+}
+console.dir(recipesList);
 function checkIngredients(recIngr, ingrsRequired) {
     if (ingrsRequired.length == 0) {
         return true;
@@ -20,8 +25,27 @@ function checkIngredients(recIngr, ingrsRequired) {
 function megaFilter(tmpPrep = 15, tmpCook = 30, nom = '', lstIngre = []) {
     let recettesOk = recipesList.filter(recette => parseInt(recette.prepTime) <= tmpPrep);
     recettesOk = recettesOk.filter(recette => parseInt(recette.cookTime) <= tmpCook);
-    recettesOk = recettesOk.filter(recette => recette.name.substring(0, nom.length).toLowerCase() == nom.toLocaleLowerCase());
-    recettesOk = recettesOk.filter(recette => (checkIngredients(recette.ingredients, lstIngre)));
+    recettesOk = recettesOk.filter(recette => recette.name.substring(0, nom.length).toLowerCase() == nom.toLowerCase());
+    recettesOk = recettesOk.filter(recette => checkIngredients(recette.ingredients, lstIngre));
     return recettesOk;
 }
-console.log("resulat   ", megaFilter(undefined, undefined, undefined, ["salt", "lemon", "garlic"]));
+function updModal(recet) {
+    console.log("From Modal");
+    console.dir(recet);
+}
+const domLstRecettes = document.querySelector('.recettes-container');
+const modalRecette = document.querySelector("dialog");
+const btnModalExit = document.getElementById("btnCloseModal");
+function mkButtonMenus(lstRecets) {
+    lstRecets.forEach(recet => {
+        const newButton = document.createElement('button');
+        newButton.innerText = recet.name;
+        newButton.onclick = () => {
+            updModal(recet);
+            modalRecette.showModal();
+        };
+        domLstRecettes.appendChild(newButton);
+    });
+}
+btnModalExit.onclick = () => { modalRecette.close(); };
+mkButtonMenus(recipesList);
--- TP-27sept/ts/main.ts
+++ TP-27sept/ts/main.ts
@@ -1,10 +1,24 @@
 import { recipes } from "./data/recipes.js";
 import { Recipe } from "./interface/recipe.js";
 
-const recipesList: Recipe[] = [] ;
 
+// récupération des recettes
+
+const recipesList: Recipe[] = [] ;
 for ( const cle in recipes ) { recipesList.push(recipes[cle]); }
 
+// récupération des clés et affectation à chaque recette
+// potentiellement inutile, je pense pouvoir m'en passer...
+
+const recettesKeys = Object.keys(recipes);
+
+for (let i = 0 ; i < recipesList.length ; i++) {
+    recipesList[i].id =recettesKeys[i];
+}
+console.dir(recipesList);
+
+
+//filtres
 
 function checkIngredients(recIngr: {}, ingrsRequired: string[]): boolean {
 
@@ -17,7 +31,7 @@ function checkIngredients(recIngr: {}, ingrsRequired: string[]): boolean {
         for ( const ind in recIngr ) { lstIngreRec.push(recIngr[ind].name) } 
         // => erreur de compilation : Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{}'.
         // No index signature with a parameter of type 'string' was found on type '{}'.ts(7053)
-        // mais ça fonctionne
+        // mais ça fonctionne...
 
         const retBool: boolean[] = [];
 
@@ -28,17 +42,62 @@ function checkIngredients(recIngr: {}, ingrsRequired: string[]): boolean {
         
 }
 
-
-
 function megaFilter(tmpPrep: number = 15, tmpCook: number = 30, nom: string = '', lstIngre: string[] = []): Recipe[] {
 
     let recettesOk = recipesList.filter( recette  => parseInt(recette.prepTime) <= tmpPrep )
     recettesOk = recettesOk.filter( recette  => parseInt(recette.cookTime) <= tmpCook )
-    recettesOk = recettesOk.filter( recette  => recette.name.substring(0,nom.length).toLowerCase() == nom.toLocaleLowerCase() )
-    recettesOk = recettesOk.filter( recette => (checkIngredients(recette.ingredients, lstIngre)) )
+    recettesOk = recettesOk.filter( recette  => recette.name.substring(0,nom.length).toLowerCase() == nom.toLowerCase() )
+    recettesOk = recettesOk.filter( recette => checkIngredients(recette.ingredients, lstIngre) )
     
     return recettesOk;
 }
 
+function updModal(recet: Recipe): void {
+    console.log("From Modal");
+    console.dir(recet);
+}
 
-console.log("resulat   ", megaFilter(undefined,undefined,undefined,["salt","lemon","garlic"]));
+//affichage des "boutons-menus"
+
+const domLstRecettes = document.querySelector('.recettes-container') as HTMLDivElement ;
+const modalRecette = document.querySelector("dialog") as HTMLDialogElement ;
+const btnModalExit = document.getElementById("btnCloseModal") as HTMLButtonElement ;
+
+function mkButtonMenus(lstRecets: Recipe[]): void {
+    lstRecets.forEach( recet => {
+        const newButton = document.createElement('button');
+        newButton.innerText = recet.name ;
+       // newButton.setAttribute("data-key",(recet.id)? recet.id : (new Date).toString()); // affectation de la clé en data-set
+        newButton.onclick = () => {
+            updModal(recet) ;      // passage de l'objet complet en paramètre, ce qui rend le data-key inutile
+            modalRecette.showModal();
+        }
+
+        domLstRecettes.appendChild(newButton);
+    })
+}
+
+btnModalExit.onclick = () => { modalRecette.close() }
+
+mkButtonMenus(recipesList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
