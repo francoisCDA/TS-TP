f6087c5743400488111e71fc9c517c9c30f5b38a
François Termote <francois.termote@gmail.com>
2023-09-27 19:11:26 +0200

    TP recettes, filtres OK, => HTML, CSS à faire

--- TP-27sept/js/main.js
+++ TP-27sept/js/main.js
@@ -3,10 +3,25 @@ const recipesList = [];
 for (const cle in recipes) {
     recipesList.push(recipes[cle]);
 }
+function checkIngredients(recIngr, ingrsRequired) {
+    if (ingrsRequired.length == 0) {
+        return true;
+    }
+    else {
+        const lstIngreRec = [];
+        for (const ind in recIngr) {
+            lstIngreRec.push(recIngr[ind].name);
+        }
+        const retBool = [];
+        ingrsRequired.forEach(ingr => { retBool.push(lstIngreRec.includes(ingr)); });
+        return !retBool.includes(false);
+    }
+}
 function megaFilter(tmpPrep = 15, tmpCook = 30, nom = '', lstIngre = []) {
     let recettesOk = recipesList.filter(recette => parseInt(recette.prepTime) <= tmpPrep);
     recettesOk = recettesOk.filter(recette => parseInt(recette.cookTime) <= tmpCook);
     recettesOk = recettesOk.filter(recette => recette.name.substring(0, nom.length).toLowerCase() == nom.toLocaleLowerCase());
+    recettesOk = recettesOk.filter(recette => (checkIngredients(recette.ingredients, lstIngre)));
     return recettesOk;
 }
-console.log(megaFilter(undefined, undefined, 'Lem', undefined));
+console.log("resulat   ", megaFilter(undefined, undefined, undefined, ["salt", "lemon", "garlic"]));
--- TP-27sept/ts/main.ts
+++ TP-27sept/ts/main.ts
@@ -6,14 +6,39 @@ const recipesList: Recipe[] = [] ;
 for ( const cle in recipes ) { recipesList.push(recipes[cle]); }
 
 
+function checkIngredients(recIngr: {}, ingrsRequired: string[]): boolean {
+
+    if (ingrsRequired.length == 0) {
+        return true ;
+    } else {
+        
+        const lstIngreRec: string[] = [];
+            
+        for ( const ind in recIngr ) { lstIngreRec.push(recIngr[ind].name) } 
+        // => erreur de compilation : Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{}'.
+        // No index signature with a parameter of type 'string' was found on type '{}'.ts(7053)
+        // mais ça fonctionne
+
+        const retBool: boolean[] = [];
+
+        ingrsRequired.forEach( ingr => { retBool.push(lstIngreRec.includes(ingr))  })
+
+        return !retBool.includes(false);
+    }
+        
+}
+
+
+
 function megaFilter(tmpPrep: number = 15, tmpCook: number = 30, nom: string = '', lstIngre: string[] = []): Recipe[] {
 
     let recettesOk = recipesList.filter( recette  => parseInt(recette.prepTime) <= tmpPrep )
     recettesOk = recettesOk.filter( recette  => parseInt(recette.cookTime) <= tmpCook )
-    recettesOk = recettesOk.filter( recette  => recette.name.substring(0,nom.length).toLowerCase() == nom.toLocaleLowerCase()  )
+    recettesOk = recettesOk.filter( recette  => recette.name.substring(0,nom.length).toLowerCase() == nom.toLocaleLowerCase() )
+    recettesOk = recettesOk.filter( recette => (checkIngredients(recette.ingredients, lstIngre)) )
     
     return recettesOk;
 }
 
 
-console.log(megaFilter(undefined,undefined,'Lem',undefined));
+console.log("resulat   ", megaFilter(undefined,undefined,undefined,["salt","lemon","garlic"]));
